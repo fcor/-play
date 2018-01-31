8a0516c6967ddbf938cb5790156bf4f5ebdd735c
Fabio Cortés <fcor14@hotmail.com>
2018-01-31 11:30:16 -0500

    Hover effect test

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -5,10 +5,15 @@ class Gif extends React.Component {
     const { img, width, left, top, right, bottom } = this.props
     return(
       <div className="gif" style={{top: top, bottom: bottom, left: left, right: right}}>
-        <img src={img}
-          alt="gif"
-          width={width}
-        />
+        <div className="gif-box">
+          <div className="gif-text">
+            Hola, aca va el link
+          </div>
+          <img src={img}
+            alt="gif"
+            width={width}
+          />
+        </div>
       </div>
     )
   }
