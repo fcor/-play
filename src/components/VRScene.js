104ee616d530fd503ccd3d8103aa672bf694712c
Fabio Cortés <fcor14@hotmail.com>
2018-02-05 17:09:55 -0500

    Firebase and IFrame support added

--- src/components/VRScene.js
+++ src/components/VRScene.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import 'aframe'
+// import 'aframe'
 
 const VRScene = () =>
-  <div id="myEmbeddedScene">
-    <a-scene embedded>
-      <a-assets>
-          <img src="https://ucarecdn.com/04e4c90c-dfcb-4747-b941-cf7682a63d2a/" id="panorama" />
-        </a-assets>
-        <a-sky src="#panorama"></a-sky>
-    </a-scene>
+  <div className="vr-content">
+    <iframe className="iframe" title="VR"
+      srcDoc='<html>
+      <head>
+        <title>VR Scene</title>
+      </head>
+      <body>
+        <script src="https://aframe.io/releases/0.7.1/aframe.min.js"></script>
+         <a-scene>
+          <a-assets>
+            <img crossOrigin="anonymous" src="https://ucarecdn.com/04e4c90c-dfcb-4747-b941-cf7682a63d2a/" id="panorama" />
+          </a-assets>
+          <a-sky src="#panorama"></a-sky>
+        </a-scene>
+      </body>
+    </html>'
+      width="100%" height="100%" allowFullScreen="yes" allowvr="yes" scrolling="no">
+
+
+    </iframe>
   </div>
+  // <div id="myEmbeddedScene">
+  // </div>
 
 
 export default VRScene
