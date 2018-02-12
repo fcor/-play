e92d9f78f5e03ec5068a17b8fa1833f5820ba3f7
Fabio Cortés <fcor14@hotmail.com>
2018-02-12 14:59:34 -0500

    Gif titles

--- src/utils/gifs.js
+++ src/utils/gifs.js
@@ -26,7 +26,7 @@ const gifs = (param) => {
       width: "250",
       route: "mundo",
       title: "Una observación al fin del mundo",
-      titleEn: "Observation to the end of the world",
+      titleEn: "An Observation of the End of the World",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -40,9 +40,9 @@ const gifs = (param) => {
       topR: "251px",
       width: "250",
       route: "planoz",
-      title: "Plano Z",
-      titleEn: "Plano Z Eng",
-      year: "2017",
+      title: "Moto Z | Plano Z",
+      titleEn: "Moto Z | Plain Z",
+      year: "2016",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
     }
@@ -55,7 +55,7 @@ const gifs = (param) => {
       topR: "40px",
       route: "topologias",
       title: "Topologías urbanas",
-      titleEn: "Urban topology",
+      titleEn: "Urban topologies",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -69,8 +69,8 @@ const gifs = (param) => {
       topR: "330px",
       width: "250",
       route: "moto",
-      title: "Moto",
-      titleEn: "Moto Eng",
+      title: "Moto Z2 Play | Circuito de Arte",
+      titleEn: "Moto Z2 Play | Art Circuit",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -83,8 +83,8 @@ const gifs = (param) => {
       leftR: "180px",
       topR: "380px",
       route: "adidas",
-      title: "Adidas",
-      titleEn: "Adidas Eng",
+      title: "Adidas Nemeziz",
+      titleEn: "Adidas Nemeziz",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -98,8 +98,8 @@ const gifs = (param) => {
       topR: "40px",
       width: "300",
       route: "sonar",
-      title: "Sonar",
-      titleEn: "Sonar Eng",
+      title: "Sonar AV Experience",
+      titleEn: "Sonar AV Experience",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -112,9 +112,9 @@ const gifs = (param) => {
       rightR: "120px",
       topR: "581px",
       route: "uber",
-      title: "Uber",
-      titleEn: "Uber Eng",
-      year: "2017",
+      title: "UberPool",
+      titleEn: "UberPool",
+      year: "2015",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
     }
@@ -126,8 +126,8 @@ const gifs = (param) => {
       rightR: "171px",
       topR: "380px",
       route: "nike",
-      title: "Nike",
-      titleEn: "Nike Eng",
+      title: "Nike Lanzamiento Air Force",
+      titleEn: "Nike Air Force Launch",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -141,8 +141,8 @@ const gifs = (param) => {
       topR: "40px",
       width: "300",
       route: "coca",
-      title: "Coca",
-      titleEn: "Coca Eng",
+      title: "De la Coca a la Pimienta",
+      titleEn: "De la Coca a la Pimienta",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -155,9 +155,9 @@ const gifs = (param) => {
       leftR: "208px",
       topR: "431px",
       route: "cuerpos",
-      title: "Cuerpos de agua",
-      titleEn: "Water",
-      year: "2017",
+      title: "Cuerpos de Agua",
+      titleEn: "Water Bodies",
+      year: "2015",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
     }
@@ -169,8 +169,8 @@ const gifs = (param) => {
       leftR: "136px",
       topR: "833px",
       route: "redbull",
-      title: "Redbull",
-      titleEn: "Redbull eng",
+      title: "Deportistas RedBull",
+      titleEn: "RedBull Athletes",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -184,8 +184,8 @@ const gifs = (param) => {
       topR: "40px",
       width: "500px",
       route: "redbullP",
-      title: "RedbullP",
-      titleEn: "RedbullP eng",
+      title: "Redbull Parapenta",
+      titleEn: "Redbull Paragliding",
       year: "2017",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
@@ -198,9 +198,9 @@ const gifs = (param) => {
       leftR: "297px",
       topR: "355px",
       route: "warm",
-      title: "Sonar warm up",
-      titleEn: "Sonar Warm up eng",
-      year: "2017",
+      title: "Sonar Warm Up",
+      titleEn: "Sonar Warm Up",
+      year: "2015",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
     }
@@ -213,9 +213,9 @@ const gifs = (param) => {
       topR: "1170px",
       width: "450",
       route: "samsung",
-      title: "Samsung",
-      titleEn: "Samsung Eng",
-      year: "2017",
+      title: "El Regalo del Mar | Samsung",
+      titleEn: "The Gift of the Sea | Samsung",
+      year: "2015",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
     }
@@ -227,9 +227,9 @@ const gifs = (param) => {
       rightR: "284px",
       topR: "942px",
       route: "freaky",
-      title: "Freaky",
-      titleEn: "Freaky Eng",
-      year: "2017",
+      title: "El Freaky ft. Mr Lexx & Cybertronics",
+      titleEn: "El Freaky ft. Mr Lexx & Cybertronics",
+      year: "2015",
       desc: "Acá va la descripción",
       descEN: "Here goes the description"
     }
