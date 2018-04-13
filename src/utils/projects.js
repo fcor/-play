90b5587269c59fd579545459bae4dffcddde8f5a
Fabio Cortés <fcor14@hotmail.com>
2018-04-13 16:22:35 -0500

    Mundo Mobile ok

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -36,6 +36,13 @@ import mundo6 from '../images/mundo/66.mp4'
 import mundo7 from '../images/mundo/7.jpg'
 import mundo8 from '../images/mundo/88.mp4'
 import mundo9 from '../images/mundo/99.mp4'
+import mundo1Mob from '../images/mundo/1Mob.mp4'
+import mundo2Mob from '../images/mundo/2Mob.mp4'
+import mundo3Mob from '../images/mundo/3Mob.mp4'
+import mundo4Mob from '../images/mundo/4Mob.mp4'
+import mundo5Mob from '../images/mundo/5Mob.jpg'
+import mundo6Mob from '../images/mundo/6Mob.mp4'
+import mundo7Mob from '../images/mundo/7Mob.mp4'
 import adidas2 from '../images/adidas/adidas_interna1.mp4'
 import adidas3 from '../images/adidas/adidas_interna2.mp4'
 import adidas4 from '../images/adidas/adidas_interna3.mp4'
@@ -203,6 +210,8 @@ const getProjectDetails = (param) => {
               Dirección: Alvaro Rodriguez, Carlos Serrano, Juan David Figueroa. Dirección de Arte: Lina Barrios. Animación 3D: Carlos Serrano, Pierre Puentes. Animación 2D: Juan David Figueroa, Daniel Shambo. Edición y Finalización: Alvaro Rodriguez, Juan David Figueroa. Música: Juan Mattos.`,
       descEn: `“Somebody is looking through a microscope, watching the world end. Slowly, understanding the vastness of existence contained inside a petri dish. Witnessing the Universe’s natural tendency into the chaos and decomposition of the systems. A tendency which manifests itself, regardless of how immense or how finite the system. For all that takes place under the microscope is nothing more than the reflection of what is projected up in the sky. An assertion, made visible under the  lens: the destruction of the organic systems and with them, the rupture of the biological world as we know it. Each cell is transformed, ignoring its integral function within the system, acting only in defense of its own survival, leading to an inevitable separation. The will of an entire system slowly becomes the individual need of a million entities willing to prevail. Finally, the energy that once united them into a same direction is dissolved and lost into randomness; into a chaos that spirals into its organic destruction as it leads to a digital construction. The digital system, which at some point functioned as an extension of the biologic, now reinforces itself exponentially, simply settling down. The entropic flow of the systems is updated with a new incline: a digitally increasing nature. Somebody is looking through a microscope, watching the world end.” \n Winner Entry at the first FullDome Festival of the Bogotá Planetarium, selected at the Immersphere Festival in Brasilia.
               Direction: Alvaro Rodriguez, Carlos Serrano, Juan David Figueroa. Art Direction: Lina Barrios. 3D Animation: Carlos Serrano, Pierre Puentes. 2D Animation: Juan David Figueroa, Daniel Shambo. Editing: Alvaro Rodriguez, Juan David Figueroa. Music: Juan Mattos.`,
+      descEsMob: `“Alguien mira a través de un microscopio, observando el fin del mundo. Lentamente, comprendiendo la vastedad de la existencia, existencia contenida en una placa de Petri. Siendo testigo de esa tendencia natural del Universo hacia el caos y la descomposición de los sistemas. Una tendencia que se manifiesta a si misma, sin importar cuan inmenso o finito es el sistema. Pues todo aquello que toma lugar bajo ese microscopio no es mas que el reflejo de lo que se proyecta arriba en el cielo. Una afirmación, hecha visible bajo el lente: la destrucción de los sistemas orgánicos y con ellos, la ruptura del mundo biológico tal y como lo conocemos. Cada célula es transformada, ignorando su función íntegra dentro del sistema, actuando solamente en defensa de su propia supervivencia, llevando así a una separación inevitable. La voluntad de un sistema entero lentamente se convierte en la necesidad individual de un millón de entidades dispuestas a prevalecer. Finalmente, la energía que alguna vez la unió hacia una misma dirección es disuelta, perdiéndose hacia el azar; hacia un caos que se desploma hacia su destrucción orgánica mientras lleva a una construcción digital. El sistema digital, que en algún punto funcionó como una extensión de lo biológico, ahora se refuerza a si mismo de manera exponencial, simplemente asentándose. El flujo entrópico de los sistemas se actualiza con una nueva inclinación: una naturaleza digital creciente. Alguien mira a través de un microscopio, observando el fin del mundo.” \n Propuesta ganadora en el primer Festival FullDome del Planetario de Bogotá, proyectada en el Festival Immersphere de Brasilia.`,
+      descEnMob: `“Somebody is looking through a microscope, watching the world end. Slowly, understanding the vastness of existence contained inside a petri dish. Witnessing the Universe’s natural tendency into the chaos and decomposition of the systems. A tendency which manifests itself, regardless of how immense or how finite the system. For all that takes place under the microscope is nothing more than the reflection of what is projected up in the sky. An assertion, made visible under the  lens: the destruction of the organic systems and with them, the rupture of the biological world as we know it. Each cell is transformed, ignoring its integral function within the system, acting only in defense of its own survival, leading to an inevitable separation. The will of an entire system slowly becomes the individual need of a million entities willing to prevail. Finally, the energy that once united them into a same direction is dissolved and lost into randomness; into a chaos that spirals into its organic destruction as it leads to a digital construction. The digital system, which at some point functioned as an extension of the biologic, now reinforces itself exponentially, simply settling down. The entropic flow of the systems is updated with a new incline: a digitally increasing nature. Somebody is looking through a microscope, watching the world end.” \n Winner Entry at the first FullDome Festival of the Bogotá Planetarium, selected at the Immersphere Festival in Brasilia.`,
       logos: [immer, idartes],
       assets: {
         a1: {
@@ -240,6 +249,36 @@ const getProjectDetails = (param) => {
         a9: {
           type: 'mp4',
           src: mundo9
+        },
+      },
+      assetsMobile: {
+        a1: {
+          type: 'mp4',
+          src: mundo1Mob
+        },
+        a2: {
+          type: 'mp4',
+          src: mundo2Mob
+        },
+        a3: {
+          type: 'mp4',
+          src: mundo3Mob
+        },
+        a4: {
+          type: 'mp4',
+          src: mundo4Mob
+        },
+        a5: {
+          type: 'img',
+          src: mundo5Mob
+        },
+        a6: {
+          type: 'mp4',
+          src: mundo6Mob
+        },
+        a7: {
+          type: 'mp4',
+          src: mundo7Mob
         },
       }
     }
